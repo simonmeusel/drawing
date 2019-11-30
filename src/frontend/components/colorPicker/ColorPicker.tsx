f1d0b79b6205cab16d027c100053c64e58091048
Simon Meusel <info@simonmeusel.de>
2019-11-30 19:01:37 +0100

    Add color picker panel

--- src/frontend/components/colorPicker/ColorPicker.tsx
+++ src/frontend/components/colorPicker/ColorPicker.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import * as React from 'react';
 import { SketchPicker } from 'react-color';
+import Draggable from 'react-draggable';
+import './ColorPicker.scss';
 
 export class ColorPicker extends React.Component {
     render() {
-        return <SketchPicker />;
+        return (
+            <div className="app-color-picker">
+                <Draggable
+                    defaultPosition={{ x: 24, y: 24 }}
+                    position={undefined}
+                    scale={1}
+                    handle=".app-color-picker-handle"
+                >
+                    <div className="card">
+                        <header className="app-color-picker-handle card-header">
+                            <p className="card-header-title">Color</p>
+                            <a
+                                href="#"
+                                className="card-header-icon"
+                                aria-label="more options"
+                            >
+                                <span className="icon">
+                                    <i
+                                        className="fas fa-angle-down"
+                                        aria-hidden="true"
+                                    ></i>
+                                </span>
+                            </a>
+                        </header>
+                        <div className="card-content">
+                            <div className="content">
+                                <SketchPicker />
+                            </div>
+                        </div>
+                    </div>
+                </Draggable>
+            </div>
+        );
     }
 }
--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,5 +1,15 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import { ColorPicker } from './components/colorPicker/ColorPicker';
+import { Canvas } from './components/canvas/Canvas';
+import 'bulma/css/bulma.min.css';
 
-ReactDOM.render(<ColorPicker />, document.getElementById('root'));
+ReactDOM.render(
+    <div style={{ position: 'relative' }}>
+        <ColorPicker />
+        <div style={{ position: 'relative' }}>
+            <Canvas />
+        </div>
+    </div>,
+    document.getElementById('root')
+);
