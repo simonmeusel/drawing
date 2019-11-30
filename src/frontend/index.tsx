6987821c48fd5c7de546c78d55b938d676cca7b9
Simon Meusel <info@simonmeusel.de>
2019-11-30 02:26:26 +0100

    Fix color picker styles

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,6 +1,5 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import { ColorPicker } from './components/colorPicker/ColorPicker';
-import '../../node_modules/rc-color-picker/assets/index.css';
 
 ReactDOM.render(<ColorPicker />, document.getElementById('root'));
