d4396da18440e9cf4d00ba386f0bf399bd10fc77
Simon Meusel <info@simonmeusel.de>
2019-12-11 22:31:23 +0100

    Add room history to reducer

--- src/frontend/store/reducers/roomID.ts
+++ src/frontend/store/reducers/roomID.ts
@@ -5,5 +5,6 @@ export function roomID(state: RootState, action: SetRoomIDAction): RootState {
     return {
         ...state,
         roomID: action.roomID,
+        roomIDHistory: state.roomIDHistory.concat([state.roomID]),
     };
 }
