f898a958671a3b24858ce8240acc520647bf006e
DoctorManhattan123 <32958091+DoctorManhattan123@users.noreply.github.com>
2021-03-28 22:31:33 +0200

    Add initial modal

--- src/frontend/components/app/welcomeModal/WelcomeModal.tsx
+++ src/frontend/components/app/welcomeModal/WelcomeModal.tsx
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css";
 import * as React from 'react'
 
 
@@ -9,20 +10,51 @@ export class WelcomeModal extends React.Component<{}, AppState> {
     state ={
         isModalOpen: true,
     }
+
+    handleClick = () => {
+        this.setState({ isModalOpen: !this.state.isModalOpen });
+    };
+    
     render() {
+        const active = this.state.isModalOpen ? "modal is-active" : "modal";
         return (
-            <div className="modal">
-                <div className="modal-background"/>
+            <div className={active}>
+                <div className="modal-background" />
                 <div className="modal-card">
                     <header className="modal-card-head">
-                        <p className="modal-card-title">Modal title</p>
-                        <button className="delete" aria-label="close"/>
+                        <p className="modal-card-title">Tutorial</p>
+                        <button
+                            onClick={this.handleClick}
+                            className="delete"
+                            aria-label="close"
+                        />
                     </header>
                     <section className="modal-card-body">
+                        <div className="field">
+                            <label className="label">Tools</label>
+                            <div className="content">
+                                In our application you can choose between different tools. You do not have to use
+                                the "Move" tool. If you are sitting at a computer you can just hold right click and move with it.
+                                If you want to change the color. The world would obviously be very sad and boring without colors,
+                                so you can choose the color of the figures. There are two possible options. The stroke color and the fill color.
+                                You can also minimize the toolbar if you want more space.
+                            </div>
+                        </div>
+                        <div className="field">
+                            <label className="label">Rooms</label>
+                            <div className="content">
+                                We have different rooms, so you do not have to look at other peoples drawings. If you want to share
+                                your room with someone just click on "Copy" and send it to the other person. If you want to create
+                                a new room just go to the dropdown and you will automatically get a new room. All the rooms the ever used
+                                will be saved in the history, you can of course delete them.
+                            </div>
+                        </div>
                     </section>
                     <footer className="modal-card-foot">
-                        <button className="button is-success">Save changes</button>
-                        <button className="button">Cancel</button>
+                        <button className="button is-success" onClick={this.handleClick}>Got it!</button>
+                        <button onClick={this.handleClick} className="button">
+                            Something smart.
+                        </button>
                     </footer>
                 </div>
             </div>
