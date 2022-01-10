72bc0178a703a9ff22352eee39288dec5935ad9b
Alexander Berkov <alexandervonberkov1@gmail.com>
2022-01-10 05:33:17 +0100

    Revert "feat: change color properties from type string to type color"
    This reverts commit 405833af995e87b435978fb8f8b046e586907406.

--- src/shared/shapes/BasicShape.ts
+++ src/shared/shapes/BasicShape.ts
@@ -1,4 +1,3 @@
-import { Color } from '../Color';
 import { FilledShape } from './FilledShape';
 import { StrokedShape } from './StrokedShape';
 
@@ -7,7 +6,7 @@ export type BasicShapeType = 'ellipse' | 'rectangle';
 export interface BasicShape extends StrokedShape, FilledShape {
     type: BasicShapeType;
     data: {
-        strokeColor: Color;
-        fillColor: Color;
+        strokeColor: string;
+        fillColor: string;
     };
 }
